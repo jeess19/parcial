959d3e97d438c9f617b366d6e17110b68cb7265a
Jessica Gaitan <gaitanjessica22@gmail.com>
2025-09-02 09:56:33 -0600

    actualizando repositor

--- viaje.py
+++ viaje.py
@@ -1,9 +1,10 @@
 # Clase para guardar la info de cada viaje
 class Viaje:
-    def _init_(self, ruta, costo, tiempo):
+    def init(self, ruta, costo, tiempo):
         self.ruta = ruta
         self.costo = costo
         self.tiempo = tiempo
 
     def mostrar(self):
         return f"Ruta: {self.ruta} | Costo: ${self.costo} | Tiempo: {self.tiempo} min"
+from viaje import Viaje
